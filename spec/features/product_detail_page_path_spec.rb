83923a5cb9c8bd2fa85e3ef6a140c45539246ffc
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-02-17 16:18:55 -0800

    pass integration spec testing the path to the product details page :

--- spec/features/product_detail_page_path_spec.rb
+++ spec/features/product_detail_page_path_spec.rb
@@ -4,7 +4,10 @@ describe "the product detail path" do
 
   it "tests the product detail route" do
     user = create(:user)
-
+    product = create(:product)
+    visit 'products#index'
+    click_on 'Go Lite Bag'
+    expect(page).to have_content 'Product Description'
   end
 
 
