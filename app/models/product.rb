cc9a7abd279ab929023710359e23d0a3d12783dd
Andrew Accuardi <andrewaccuardi@gmail.com>
2017-07-25 16:27:10 -0700

    Clean up model code.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
-
   has_attached_file :picture, :styles => { full: "500x500>", medium: "300x300", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  validates :name, :description, :price, presence: :true
 
   extend FriendlyId
   friendly_id :name, use: :slugged
@@ -11,5 +11,4 @@ class Product < ApplicationRecord
   has_many :users, through: :comments
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :builds
-  validates :name, :description, :price, presence: :true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   extend FriendlyId
   friendly_id :username, use: :slugged
 
